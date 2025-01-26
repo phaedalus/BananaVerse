8d3ac9b42eaea7ab19c7a07ec4d3608011591bbb
Phaedalus <thephaedalus@gmail.com>
2025-01-26 16:05:32 -0600

    2.2 - New Frontiers
    Alias' make the return!
    Support for GTA5 PS5 Version!

--- js/essential/render-game.js
+++ js/essential/render-game.js
@@ -3,15 +3,21 @@ function renderGame(gameAbr) {
     let gameD = sortedCharacters[gameAbr].dead;
     let gameAliveList = document.getElementById(`${gameAbr}-Alive`);
     let gameDeadList = document.getElementById(`${gameAbr}-Dead`);
+    
     gameA.forEach((character, index) => {
         let li = document.createElement("li");
-        li.innerText = character.fullname;
+        li.innerText = character.alias 
+            ? `${character.fullname} (${character.alias})` 
+            : character.fullname;
         li.onclick = () => openProfile(character);
         gameAliveList.appendChild(li);
     });
+
     gameD.forEach((character, index) => {
         let li = document.createElement("li");
-        li.innerText = character.fullname;
+        li.innerText = character.alias 
+            ? `${character.fullname} (${character.alias})` 
+            : character.fullname;
         li.onclick = () => openProfile(character);
         gameDeadList.appendChild(li);
     });
---
+++ js/game/gta5p5.js
@@ -0,0 +1,2 @@
+const gta5p5Button = document.getElementById("gta-5-p5");
+gta5p5Button.addEventListener("click", () => displayCharacters("GTA5P5"));
